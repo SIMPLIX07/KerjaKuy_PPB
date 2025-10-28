68b1a0c0d307a6f46897954718e60fc9147daad8
SIMPLIX07 <muhammad99salman98@gmail.com>
2025-10-28 13:07:07 +0700

    page 1 10%

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      theme: ThemeData(
+        useMaterial3: false,
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Colors.white,
+        ),
+      ),
+      home: const HomePage(),
     );
   }
 }
@@ -40,69 +46,93 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: PageView(
           controller: _pageController,
           onPageChanged: _onPageChanged,
-          children: const [
-            Center(
-              child: Text(
-                "Page 1",
-                style: TextStyle(
-                  fontSize: 24)
-                  )
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
               children: [
-                Text(
-                  "Test Safe   Area",
-                  style: TextStyle(
-                    fontSize: 18, 
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-                Expanded(
-                  child: Center(
-                    child: Text(
-                      "Page 2",
-                      style: TextStyle(
-                        fontSize: 24)
+                Align(
+                  alignment: Alignment.topLeft, 
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment:
+                          CrossAxisAlignment.center, 
+                      children: [
+                        Container(
+                          width: 20,
+                          height: 20,
+                          decoration: const BoxDecoration(
+                            color: Colors.green,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                        const SizedBox(width: 10),
+                        const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Muhammad Salman Al Farizy",
+                              style: TextStyle(fontSize: 9),
+                            ),
+                            Text(
+                              "Calon Karyawan",
+                              style: TextStyle(fontSize: 8, color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                        Text("data")
+                      ],
                     ),
                   ),
                 ),
               ],
             ),
-            Center(
-              child: Text(
-                "Page 3",
-                style: TextStyle(
-                  fontSize: 24
-                  )
-              )
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Test Safe   Area",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text("Page 2", style: TextStyle(fontSize: 24)),
+                  ),
+                ),
+              ],
             ),
-            Center(
-              child: Text(
-                "Page 3",
-                style: TextStyle(
-                  fontSize: 24
-                  )
-              )
-            ),
+            Center(child: Text("Page 3", style: TextStyle(fontSize: 24))),
+            Center(child: Text("Page 3", style: TextStyle(fontSize: 24))),
           ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: _onItemTapped,
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.grey,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Page 1"),
-          BottomNavigationBarItem(icon: Icon(Icons.work), label: "Page 2"),
-          BottomNavigationBarItem(icon: Icon(Icons.schedule), label: "Page 3"),
-          BottomNavigationBarItem(icon: Icon(Icons.task), label: "Page 4"),
-        ],
+      bottomNavigationBar: PhysicalModel(
+        color: Colors.white,
+        elevation: 8,
+        shadowColor: Colors.black,
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          currentIndex: _currentIndex,
+          onTap: _onItemTapped,
+          selectedItemColor: Colors.green,
+          unselectedItemColor: Colors.grey,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Page 1"),
+            BottomNavigationBarItem(icon: Icon(Icons.work), label: "Page 2"),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.schedule),
+              label: "Page 3",
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.task), label: "Page 4"),
+          ],
+        ),
       ),
     );
   }
